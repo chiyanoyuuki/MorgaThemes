d78068966257b5d485915b300b96cc666fa76dd2
chiyanoyuuki <charles.poure@hotmail.com>
2024-08-31 14:50:31 +0200

    23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, ElementRef, NgModule, OnInit, ViewChild } from '@angular/cor
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import axios from 'axios';
-import data from "../assets/data.json";
+import { HttpClient } from '@angular/common/http';
+import * as INFOS from '../assets/data.json';
 
 
 interface BirthData {
@@ -33,24 +33,86 @@ export class AppComponent implements OnInit
   @ViewChild('one', {static: true}) d1: ElementRef;
   focus:any = [];
 
-  data:any = data;
+  infos: any = INFOS;
+
+  hover:any = "Sélectionnez un point";
   svg: any = [];
   svgid:any="";
   maisons:any = [];
   planetes:any = [];
   svgs: any;
   fileContent: any;
+  clicked:any;
+  data: any = [];
+  informations: any = {};
+  desc: any;
 
   API_URL = 'https://api.openai.com/v1/chat/completions';
+  
+  public innerWidth: any = window.outerWidth;
+  public innerHeight: any = window.outerHeight;
 
-  constructor(private elementRef: ElementRef<HTMLElement>) {}
+  constructor(private http : HttpClient) {}
   
   ngOnInit(): void 
   {
-    this.fileContent = this.data;
-    this.format();
-    //this.test().then((r)=>console.log(r));
-    
+    this.readFile();
+  }
+
+  readFile()
+  {
+    this.http.get("../assets/charles.txt",{responseType: 'text'}).subscribe(text => {
+      this.fileContent = text;
+      this.format();
+    });
+  }
+
+  readSigneGpt()
+  {
+    let signe = "Balance";
+
+    this.http.get("../assets/signe.txt",{responseType: 'text'}).subscribe(text => {
+      let lignes = text.split("\r\n");
+      lignes = lignes.filter((ligne:any)=>ligne.length>1);
+      let planetes:any = {nom:"Planetes",data:[]};
+      let asteroides:any = {nom:"Asteroides",data:[]};
+      let maisons:any = {nom:"Maisons",data:[]};
+      let type;
+      let data:any;
+      for(let i=0;i<lignes.length;i++)
+      {
+        let ligne = lignes[i];
+        if(ligne=="Planetes")type = "Planetes";
+        else if(ligne=="Asteroides")
+        {
+          planetes.data.push(data);
+          data = undefined;
+          type = "Asteroides";
+        }
+        else if(ligne=="Maisons")
+        {
+          asteroides.data.push(data);
+          data = undefined;
+          type = "Maisons";
+        }
+        else if(!ligne.startsWith(" "))
+        {
+          if(data){
+            if(type=="Planetes")planetes.data.push(data);
+            else if(type=="Asteroides")asteroides.data.push(data);
+            else if(type=="Maisons")maisons.data.push(data);
+          }
+          data = {nom:ligne,data:[]};
+        }
+        else
+        {
+          let infos = ligne.split(":");
+          data.data.push({nom:infos[0],data:infos[1]});
+        }
+      }
+      maisons.data.push(data);
+      console.log({nom:signe,planetes:planetes,asteroides:asteroides,maisons:maisons});
+    });
   }
 
   /*async getChatGPTResponse(nom: string,isPlanet:boolean): Promise<string> {
@@ -114,145 +176,70 @@ processFileContent(): void {
 
   format()
   {
-    //let code = this.fileContent.split("\r\n");
-    console.log(this.fileContent);
-    let code = this.fileContent;
-    let rgx = new RegExp(".*<div.* en (M|m)aison .*", 'i');
+    let code = this.fileContent.split("\r\n");
+
+    let rgx = new RegExp(".*<div.*", 'g');
     let lignes = code.filter((c:any)=>c.match(rgx));
+    rgx = new RegExp(".*<div.* en (M|m)aison .*");
 
     lignes.forEach((s:any)=>{
-        s = s.substring(s.indexOf(">")+1);
-				s = s.substring(0,s.indexOf("<"));
-				s = s.replace(/ +/g, " ");
+      s = s.replace(/ Я /," ");
 
-        let nom2 = s.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/°/g,"degres");
-			let nom = nom2;
-			nom = nom.replace(/Я/g, "");
-			nom = nom.replace(/°/g, "deg");
-			nom = nom.replace(/'/g, "min");
-			nom = nom.replace(/ . /g, " ");
-			nom = nom.replace(/ +/g, " ");
-			nom = nom.replace(/\\p{M}/g, "");
-			
-			let maison = nom.substring(nom.indexOf("en Maison")+3);
-			let signe = nom.substring(0,nom.indexOf("en Maison")-1);
-			signe = signe.substring(signe.lastIndexOf(" ")+1);
-			let degres = nom.substring(0,nom.indexOf("en Maison")-1);
-			degres = degres.substring(0,degres.lastIndexOf(" "));
-			degres = degres.substring(degres.lastIndexOf(" ")+1);
-			let planete = nom.substring(0,nom.indexOf("en Maison")-1);
-			planete = planete.substring(0,planete.lastIndexOf(" "));
-			planete = planete.substring(0,planete.lastIndexOf(" "));
-
-      let obj:any = {nom:nom2,type:"Planete",signe:signe,degres:degres,maison:maison,planete:planete,id:"objet"+this.signes(planete)};
-      this.planetes.push(obj);   
-      /*this.getChatGPTResponse(obj.nom,true).then(response => {
-        obj.gpt = response.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/°/g,"degres");
-      })
-      .catch(error => {
-          console.error('Error:', error);
-      });*/
-			
-    });
-
-    rgx = new RegExp(".*>(M|m)aison .*°.*", 'i');
-      lignes = code.filter((c:any)=>c.match(rgx));
-      lignes.forEach((s:any)=>{
-        s = s.substring(s.indexOf(">")+1);
-				s = s.substring(0,s.indexOf("<"));
-				s = s.replace(/ +/g, " ");
-        console.log(s);
-
-        let nom2 = s.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/°/g,"degres");
-        let nom = nom2;
-        nom = nom.replace(/Я/g, "");
-        nom = nom.replace(/°/g, "deg");
-        nom = nom.replace(/'/g, "min");
-        nom = nom.replace(/ . /g, " ");
-        nom = nom.replace(/ +/g, " ");
-        nom = nom.replace(/\\p{M}/g, "");
-			let signe = nom.substring(nom.lastIndexOf(" ")+1);
-			let degres = nom.substring(0,nom.lastIndexOf(" "));
-			degres = degres.substring(degres.lastIndexOf(" ")+1);
-			let maison = nom.substring(0,nom.lastIndexOf(" "));
-			maison = maison.substring(0,maison.lastIndexOf(" "));
-
-      //id des maisons
-      //get gpt au clic
-      
-      let obj : any = {nom:nom2,type:"Maison",signe:signe,degres:degres,maison:maison,id:"objet"+this.signes(signe)};
-			if(!this.planetes.find((p:any)=>obj.nom==p.nom))
+      if(s.includes(" en Maison "))
       {
-        this.planetes.push(obj); 
-        /*this.getChatGPTResponse(obj.nom,false).then(response => {
-          obj.gpt = response.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/°/g,"degres");
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });*/
-        
-      }
-      });  
-
-      rgx = new RegExp(".*(>(M|m)ilieu du (C|c)iel .*°|>(A|a)scendant .*°).*", 'i');
-      lignes = code.filter((c:any)=>c.match(rgx));
-      lignes.forEach((s:any)=>{
         s = s.substring(s.indexOf(">")+1);
-				s = s.substring(0,s.indexOf("<"));
-				s = s.replace(/ +/g, " ");
-        console.log(s);
-
-        let nom2 = s.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/°/g,"degres");
-        let nom = nom2;
-        nom = nom.replace(/Я/g, "");
-        nom = nom.replace(/°/g, "deg");
-        nom = nom.replace(/'/g, "min");
-        nom = nom.replace(/ . /g, " ");
-        nom = nom.replace(/ +/g, " ");
-        nom = nom.replace(/\\p{M}/g, "");
-			let signe = nom.substring(nom.lastIndexOf(" ")+1);
-			let degres = nom.substring(0,nom.lastIndexOf(" "));
-			degres = degres.substring(degres.lastIndexOf(" ")+1);
-			let maison = nom.substring(0,nom.lastIndexOf(" "));
-			maison = maison.substring(0,maison.lastIndexOf(" "));
-
-      //id des maisons
-      //get gpt au clic
-      
-      let obj : any = {nom:nom2,type:"Planete",signe:signe,degres:degres,planete:maison,id:"objet"+this.signes(maison)};
-			if(!this.planetes.find((p:any)=>obj.nom==p.nom))
-      {
-        this.planetes.push(obj); 
-        /*this.getChatGPTResponse(obj.nom,false).then(response => {
-          obj.gpt = response.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/°/g,"degres");
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });*/
+        s = s.substring(0,s.indexOf("<"));
         
+        let signe = s.substring(0,s.indexOf(","));
+        signe = signe.substring(signe.lastIndexOf(" ")+1);
+
+        let nom = s.substring(0,s.indexOf(","));
+        nom = nom.substring(0,nom.lastIndexOf(" "));
+        nom = nom.substring(0,nom.lastIndexOf(" "));
+        nom = nom.replace(/( vraie| vrai)/g,"");
+
+        let maison = s.substring(s.indexOf(",")+2);
+        maison = maison.substring(maison.indexOf(" ")+1);
+
+        this.data.push({nom:nom,signe:signe,maison:maison});
       }
-      });  
+      else if(s.includes(">Maison ")||s.includes(">Milieu du Ciel ")||s.includes(">Ascendant"))
+      {
+        s = s.substring(s.indexOf(">")+1);
+        s = s.substring(0,s.indexOf("<"));
 
-    /*
-    rgx = new RegExp(".*<text id=\"text.*", 'i');
-    lignes = code.filter((c:any)=>c.match(rgx));
+        let signe = s.substring(s.lastIndexOf(" ")+1);
 
-    lignes.forEach((ligne:any)=>{
-      let maison = ligne;
-				maison = maison.substring(maison.indexOf(">")+1);
-				maison = maison.substring(0,maison.indexOf("<"));
-				
-				let x = ligne;
-				x = x.substring(x.indexOf("x=\"")+3);
-				x = x.substring(0,x.indexOf("\""));
-				
-				let y = ligne;
-				y = y.substring(y.indexOf("y=\"")+3);
-				y = y.substring(0,y.indexOf("\""));
-				
-				this.planetes.push(this.getJson(maison,x,y,"Maison","x"));
+        let nom = s.substring(0,s.lastIndexOf(" "));
+        nom = nom.substring(0,nom.lastIndexOf(" "));
+
+        if(nom.includes("Ascendant"))nom = "Maison I";
+        else if(nom.includes("Milieu du Ciel"))nom = "Maison X";
+
+        if(!this.data.find((d:any)=>d.signe==signe&&d.maison==nom&&!d.nom))this.data.push({signe:signe,maison:nom});
+      }
+      else if(s.includes("<div class=\"identiteNaissance\">"))
+      {
+        let prenom = s.substring(s.indexOf(">")+1);
+        prenom = prenom.substring(0,prenom.indexOf("<"));
+        let idx = lignes.indexOf(s);
+        let date = lignes[idx+1];
+        date = date.substring(date.indexOf(">")+1);
+        date = date.substring(date.indexOf(">")+1);
+        date = date.substring(0,date.indexOf(","));
+        let heure = lignes[idx+1];
+        heure = heure.substring(heure.indexOf(">")+1);
+        heure = heure.substring(heure.indexOf(">")+1);
+        heure = heure.substring(heure.indexOf(",")+1);
+        heure = heure.substring(0,heure.indexOf("<"));
+        let lieu = lignes[idx+1];
+        lieu = lieu.substring(lieu.indexOf(",")+1);
+        lieu = lieu.substring(lieu.indexOf(">")+1);
+        lieu = lieu.substring(lieu.indexOf(">")+1);
+        lieu = lieu.substring(0,lieu.indexOf(","));
+        this.informations = {prenom:prenom,date:date,heure:heure,lieu:lieu};
+      }
     });
-    */
 
     rgx = new RegExp(".*<svg id=\".*", 'i');
     let tmp = code.find((ligne:any)=>ligne.match(rgx));
@@ -262,11 +249,9 @@ processFileContent(): void {
     tmp = svgs.find((ligne:any)=>ligne.match(rgx));
     let idxend = svgs.indexOf(tmp!);
     svgs = svgs.slice(0,idxend+1);
-
     this.svg = svgs;
-    console.log(this.planetes);
-    console.log(this.svg);
 
+    console.log("this.data",this.data);
     this.init(true);
   }
 
@@ -277,77 +262,99 @@ processFileContent(): void {
 
   signes(objet: any)
 	{
+    objet = objet.replace(/[^0-9]/g,"");
 		switch(objet)
 		{
-			case "Belier":
-          return "0";
-      case "Taureau":
-          return "1";
-      case "Gemeaux":
-          return "2";
-      case "Cancer":
-          return "3";
-      case "Lion":
-          return "4";
-      case "Vierge":
-          return "5";
-      case "Balance":
-          return "6";
-      case "Scorpion":
-          return "7";
-      case "Sagittaire":
-          return "8";
-      case "Capricorne":
-          return "9";
-      case "Verseau":
-          return "10";
-      case "Poissons":
-          return "11";
-      case "Soleil":
-          return "12";
-      case "Lune":
-          return "13";
-      case "Mercure":
-          return "14";
-      case "Venus":
-          return "15";
-      case "Mars":
-          return "16";
-      case "Jupiter":
-          return "17";
-      case "Saturne":
-          return "18";
-      case "Uranus":
-          return "19";
-      case "Neptune":
-          return "20";
-      case "Pluton":
-          return "21";
-      case "Chiron":
-          return "22";
-      case "Ceres":
-          return "23";
-      case "Pallas":
-          return "24";
-      case "Junon":
-          return "25";
-      case "Vesta":
-          return "26";
-      case "Noeud Nord vrai":
-      case "Nœud Nord vrai":
-          return "27";
-      case "Lilith vraie":
-          return "28";
-      case "Fortune":
-          return "29";
-      case "Ascendant":
-          return "30";
-      case "Milieu du Ciel":
-          return "31";
-      case "Vertex":
-          return "44";
-      case "Point Est":
-          return "45";
+			case "0":
+          return "Bélier";
+      case "1":
+          return "Taureau";
+      case "2":
+          return "Gémeaux";
+      case "3":
+          return "Cancer";
+      case "4":
+          return "Lion";
+      case "5":
+          return "Vierge";
+      case "6":
+          return "Balance";
+      case "7":
+          return "Scorpion";
+      case "8":
+          return "Sagittaire";
+      case "9":
+          return "Capricorne";
+      case "10":
+          return "Verseau";
+      case "11":
+          return "Poissons";
+      case "12":
+          return "Soleil";
+      case "13":
+          return "Lune";
+      case "14":
+          return "Mercure";
+      case "15":
+          return "Vénus";
+      case "16":
+          return "Mars";
+      case "17":
+          return "Jupiter";
+      case "18":
+          return "Saturne";
+      case "19":
+          return "Uranus";
+      case "20":
+          return "Neptune";
+      case "21":
+          return "Pluton";
+      case "22":
+          return "Chiron";
+      case "23":
+          return "Cérès";
+      case "24":
+          return "Pallas";
+      case "25":
+          return "Junon";
+      case "26":
+          return "Vesta";
+      case "27":
+          return "Nœud Nord";
+      case "28":
+          return "Lilith";
+      case "29":
+          return "Fortune";
+      case "30":
+      case "32":
+          return "Maison I";
+      case "33":
+          return "Maison II";
+      case "34":
+          return "Maison III";
+      case "35":
+          return "Maison IV";
+      case "36":
+          return "Maison V";
+      case "37":
+          return "Maison VI";
+      case "38":
+          return "Maison VII";
+      case "39":
+          return "Maison VIII";
+      case "40":
+          return "Maison IX";
+      case "31":
+      case "41":
+          return "Maison X";
+      case "42":
+          return "Maison XI";
+      case "43":
+          return "Maison XII";
+      case "44":
+          return "Vertex";
+      case "45":
+          return "Point Est";
 		}
 		return "NA";
 	}
@@ -378,28 +385,29 @@ processFileContent(): void {
     for(let i=1;i<this.svg.length;i++)
       {
         let s = this.svg[i];
-        if(s.match(/.*<image.*/g))
+        if(s.match(/.*<image.*/g) || s.match(/.*<text id=\"texte.*/g))
         {
           let nom = s.substring(s.indexOf("\"")+1);
           nom = nom.substring(0,nom.indexOf("\""));
+          let signe = this.signes(nom);
           let objet:any = document.getElementById(nom);
-          objet.addEventListener('click', () => {this.click(nom)});
-          objet.addEventListener('mouseover', (event:any) => {this.addClass(nom)});
-          this.svgs.push({nom:nom});
+          objet.addEventListener('click', () => {this.click(signe)});
+          objet.addEventListener('mouseover', (event:any) => {this.hover=signe ;this.addClass(nom)});
+          this.svgs.push({nom:signe,id:nom});
         }
       }
 
 
-      console.log("PLANETES");
+      //console.log("PLANETES");
       this.svgs.forEach((svg:any)=>{
-        svg.active = this.planetes.find((planete:any)=>planete.id==svg.nom) != undefined;
+        svg.active = this.data.find((d:any)=>d.nom==svg.nom||d.signe==svg.nom||d.maison==svg.nom) != undefined;
         if(!svg.active)
         {
-          let objet:any = document.getElementById(svg.nom);
+          let objet:any = document.getElementById(svg.id);
           objet.classList.add("disabled");
         }
       })
-      console.log(this.svgs);
+      console.log("svg",this.svgs);
   }
 
   ngAfterViewInit() {
@@ -412,7 +420,7 @@ processFileContent(): void {
     for(let i=1;i<this.svg.length;i++)
       {
         let x = this.svg[i];
-        if(x.match(/.*<image.*/g))
+        if(x.match(/.*<image.*/g) || x.match(/.*<text id=\"texte.*/g))
         {
           let nom = x.substring(x.indexOf("\"")+1);
           nom = nom.substring(0,nom.indexOf("\""));
@@ -427,9 +435,50 @@ processFileContent(): void {
 
   click(s:string)
   {
-    let planetes = this.planetes.filter((p:any)=>p.id==s);
-    console.log(planetes);
-    this.focus = planetes;
+    console.log("click",s);
+    this.clicked = s;
+    this.desc = this.infos.desc.find((d:any)=>d.nom == s).infos;
+    console.log(this.infos.data);
+    let signe = this.infos.data.find((i:any)=>i.nom==s);
+    console.log(signe);
+    if(!signe)
+    {
+      let ligne = this.data.find((d:any)=>d.maison==s&&!d.nom);
+      if(ligne)
+      {
+        signe = this.infos.data.find((i:any)=>i.nom==ligne.signe).data;
+      }
+      else
+      {
+        ligne = this.data.find((d:any)=>d.nom==s);
+        signe = this.infos.data.find((i:any)=>i.nom==ligne.signe).data;
+      }
+      console.log(ligne);
+      console.log(signe);
+      let infos = signe.find((d:any)=>d.nom==s);
+      this.focus = [{nom:infos.nom,datas:infos.data}];
+      console.log(this.focus);
+      return;
+    }
+    signe = signe.data;
+    let data = this.data.filter((d:any)=>d.nom==s||d.signe==s||d.maison==s);
+    console.log(data);
+    data.forEach((d:any)=>{
+      let datas: any = [];
+      let infos = signe.find((s:any)=>s.nom==d.nom);
+      if(infos)
+      {
+        infos.data.forEach((a:any)=>datas.push(a));
+      }
+      else
+      {
+        let maison = signe.find((s:any)=>s.nom==d.maison);
+        maison.data.forEach((a:any)=>datas.push(a));
+      }
+      
+      d.datas = datas;
+    });
+    this.focus = data;
   }
 }
 
